7c36e2d3282853b5ad4a6b0b45afec3e86eab3fb
Sanjeet <sanjeet@earthrise.local>
2019-07-02 13:26:47 -0700

    comments

--- Program.cs
+++ Program.cs
@@ -8,96 +8,87 @@ using Amazon.Translate;
 using Amazon.Translate.Model;
 using Microsoft.Extensions.Configuration;
 
-namespace amazon_translate
-{
-    class Program
-    {
-        const string EnglishText = @"Amazon Translate is a text translation service that uses advanced machine learning technologies to provide high-quality translation on demand.";
-        const string TerminologyName = "Preserve_Amazon_Translate";
-        static void Main(string[] args)
-        {
-            var awsOptions = BuildAwsOptions();
-            var service = new TranslateService(awsOptions.CreateServiceClient<IAmazonTranslate>());
+namespace amazon_translate {
+	class Program {
+		const string EnglishText = @"Amazon Translate is a text translation service that uses advanced machine learning technologies to provide high-quality translation on demand.";
+		const string TerminologyName = "Preserve_Amazon_Translate";
+		static void Main(string[] args) {
+			var awsOptions = BuildAwsOptions();
+			var service = new TranslateService(awsOptions.CreateServiceClient<IAmazonTranslate>());
 
-            // simple translation
-            var translateTask = service.TranslateText(EnglishText, "en", "hi");
-            translateTask.Wait();
-            var result = translateTask.Result;
-            var translatedText = result.TranslatedText;
-            Console.WriteLine("Source: {0}", EnglishText);
-            Console.WriteLine("Translation: {0}", translatedText);
+			// simple translation
+			var translateTask = service.TranslateText(EnglishText, "en", "hi");
+			translateTask.Wait();
+			var result = translateTask.Result;
+			var translatedText = result.TranslatedText;
+			Console.WriteLine("Source: {0}", EnglishText);
+			Console.WriteLine("Translation: {0}", translatedText);
 
 
-            // translation with custom Terminology
-            var memoryStream = new MemoryStream();
-            var fileStream = new FileStream("custom-terminology.csv", FileMode.Open);
-            fileStream.CopyTo(memoryStream);
+			// translation with custom Terminology
+			// read the csv terminology file
+			var memoryStream = new MemoryStream();
+			var fileStream = new FileStream("custom-terminology.csv", FileMode.Open);
+			fileStream.CopyTo(memoryStream);
 
-            Console.WriteLine("Setting a custom Terminology...");
-            service.SetTerminolgies(TerminologyName, memoryStream).Wait();
-            var terminologies = new List<string>() { TerminologyName };
-            translateTask = service.TranslateText(EnglishText, "en", "hi", terminologies);
-            result = translateTask.Result;
-            translatedText = result.TranslatedText;
-            Console.WriteLine("After applying custom terminology:");
-            translateTask.Result.AppliedTerminologies.ForEach(x =>
-            {
-                Console.WriteLine(x.Name);
-            });
-            Console.WriteLine("Translation: {0}", translatedText);
-            fileStream.Close();
-            fileStream.Dispose();
-        }
+			// set terminology
+			Console.WriteLine("Setting a custom Terminology...");
+			service.SetTerminolgies(TerminologyName, memoryStream).Wait();
 
-        private static AWSOptions BuildAwsOptions()
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Environment.CurrentDirectory)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-            return builder.GetAWSOptions();
-        }
-    }
+			// query with terminology
+			var terminologies = new List<string>() { TerminologyName };
+			translateTask = service.TranslateText(EnglishText, "en", "hi", terminologies);
+			result = translateTask.Result;
+			translatedText = result.TranslatedText;
+			Console.WriteLine("After applying custom terminology:");
+			translateTask.Result.AppliedTerminologies.ForEach(x => {
+				Console.WriteLine(x.Name);
+			});
+			Console.WriteLine("Translation: {0}", translatedText);
+			fileStream.Close();
+			fileStream.Dispose();
+		}
 
-    public class TranslateService
-    {
-        private IAmazonTranslate translate;
-        public TranslateService(IAmazonTranslate translate)
-        {
-            this.translate = translate;
-        }
+		private static AWSOptions BuildAwsOptions() {
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Environment.CurrentDirectory)
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.Build();
+			return builder.GetAWSOptions();
+		}
+	}
 
-        public async Task<ImportTerminologyResponse> SetTerminolgies(string name, MemoryStream fileStream)
-        {
-            return await this.translate.ImportTerminologyAsync(new ImportTerminologyRequest
-            {
-                Name = name,
-                MergeStrategy = MergeStrategy.OVERWRITE,
-                TerminologyData = new TerminologyData
-                {
-                    File = fileStream,
-                    Format = TerminologyDataFormat.CSV
-                }
-            });
-        }
+	public class TranslateService {
+		private IAmazonTranslate translate;
+		public TranslateService(IAmazonTranslate translate) {
+			this.translate = translate;
+		}
 
-        public async Task<TranslateTextResponse> TranslateText(string text, string sourceLanguage, string targetLanguage)
-        {
-            return await this.TranslateText(text, sourceLanguage, targetLanguage, null);
-        }
+		public async Task<ImportTerminologyResponse> SetTerminolgies(string name, MemoryStream fileStream) {
+			return await this.translate.ImportTerminologyAsync(new ImportTerminologyRequest {
+				Name = name,
+				MergeStrategy = MergeStrategy.OVERWRITE,
+				TerminologyData = new TerminologyData {
+					File = fileStream,
+					Format = TerminologyDataFormat.CSV
+				}
+			});
+		}
 
-        public async Task<TranslateTextResponse> TranslateText(string text, string sourceLanguage, string targetLanguage, List<string> terminologies)
-        {
-            var request = new TranslateTextRequest
-            {
-                SourceLanguageCode = sourceLanguage,
-                TargetLanguageCode = targetLanguage,
-                TerminologyNames = terminologies,
-                Text = text
-            };
+		public async Task<TranslateTextResponse> TranslateText(string text, string sourceLanguage, string targetLanguage) {
+			return await this.TranslateText(text, sourceLanguage, targetLanguage, null);
+		}
 
-            return await this.translate.TranslateTextAsync(request);
-        }
-    }
+		public async Task<TranslateTextResponse> TranslateText(string text, string sourceLanguage, string targetLanguage, List<string> terminologies) {
+			var request = new TranslateTextRequest {
+				SourceLanguageCode = sourceLanguage,
+				TargetLanguageCode = targetLanguage,
+				TerminologyNames = terminologies,
+				Text = text
+			};
+
+			return await this.translate.TranslateTextAsync(request);
+		}
+	}
 
 }
